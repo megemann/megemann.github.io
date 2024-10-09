9e88fe487185d653c1534b20dcfdb579d1ced0bc
Austin Fairbanks <austinjf2005@gmail.com>
2024-10-09 13:23:03 -0400

    Fix responsiveness and Disable for mobile devices

--- personalportfolio/src/pages/projectList/projectList.style.js
+++ personalportfolio/src/pages/projectList/projectList.style.js
@@ -5,39 +5,70 @@ const s = {
         display: 'flex',
         backgroundColor: "#2E2C31",
         flexDirection: "column",
+        '@media (max-width: 768px)': {
+            minHeight: '100vh',
+            width: '100%',
+        },
     },
     backBtn: {
         width: "5vw",
         color: "red",
         marginTop: "1%",
+        '@media (max-width: 768px)': {
+            width: "10vw",
+        },
     },
     title: {
         minWidth: "20%",
         color: "#0096ff",
-        cursor: "pointer"
+        cursor: "pointer",
+        '@media (max-width: 768px)': {
+            minWidth: "40%",
+            fontSize: "16px",
+        },
     },
     link: {
         minWidth: "10%",
         textAlign: "center",
         color: "#e64e4e",
+        '@media (max-width: 768px)': {
+            minWidth: "20%",
+            fontSize: "14px",
+        },
     },
     skills: {
         minWidth: "40%",
+        '@media (max-width: 768px)': {
+            minWidth: "70%",
+            fontSize: "14px",
+        },
     },
     startdate: {
         minWidth: "10%",
-        textAlign: "center"
+        textAlign: "center",
+        '@media (max-width: 768px)': {
+            minWidth: "20%",
+            fontSize: "14px",
+        },
     },
     enddate: {
         minWidth: "10%",
-        textAlign: "center"
+        textAlign: "center",
+        '@media (max-width: 768px)': {
+            minWidth: "20%",
+            fontSize: "14px",
+        },
     },
     list: {
         margin: "1%", 
         padding: "0.5%", 
         border: "1px solid white", 
         borderRadius: "5px", 
-        boxShadow: "10px 16px 30px 0px rgba(0,0,0,0.60)"
+        boxShadow: "10px 16px 30px 0px rgba(0,0,0,0.60)",
+        '@media (max-width: 768px)': {
+            padding: "2%",
+            boxShadow: "5px 8px 15px 0px rgba(0,0,0,0.60)",
+        },
     }
 }
 
